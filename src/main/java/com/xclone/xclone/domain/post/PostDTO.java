5835c9f901ba087670b42289e46ced16869ee9dd
jokerhutt <davidglogowski2003@gmail.com>
2025-06-02 23:56:47 +0200

    feat(PostDTO) add likedBy array to PostDTO

--- src/main/java/com/xclone/xclone/domain/post/PostDTO.java
+++ src/main/java/com/xclone/xclone/domain/post/PostDTO.java
@@ -7,10 +7,12 @@ public class PostDTO {
     public Integer id;
     public Integer userId;
     public String text;
+    public ArrayList<Integer> likedBy;
 
-    public PostDTO(Post post) {
+    public PostDTO(Post post, ArrayList<Integer> likedBy) {
         this.id = post.getId();
         this.userId = post.getUserId();
         this.text = post.getText();
+        this.likedBy = likedBy;
     }
 }
