b77f514929db84d4053fb4e6f9c63d4e75aa05c4
Dave <davidglogowski2003@gmail.com>
2025-06-12 01:25:37 +0200

    feat(Retweets): add API methods

---
+++ src/main/java/com/xclone/xclone/domain/retweet/RetweetController.java
@@ -0,0 +1,34 @@
+package com.xclone.xclone.domain.retweet;
+
+import com.xclone.xclone.domain.post.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/retweets")
+public class RetweetController {
+
+    private final RetweetService retweetService;
+    private final PostService postService;
+
+    public RetweetController(RetweetService retweetService, PostService postService) {
+        this.retweetService = retweetService;
+        this.postService = postService;
+    }
+
+    @PostMapping("/newRetweet")
+    public ResponseEntity<?> newRetweet(@RequestBody NewRetweet newRetweet) {
+        retweetService.createRetweet(newRetweet);
+        return ResponseEntity.ok(postService.findPostDTOById(newRetweet.referenceId));
+    }
+
+    @PostMapping("/deleteRetweet")
+    public ResponseEntity<?> deleteRetweet(@RequestBody NewRetweet retweet) {
+        retweetService.deleteRetweet(retweet);
+        return ResponseEntity.ok(postService.findPostDTOById(retweet.referenceId));
+    }
+
+}
--- src/main/java/com/xclone/xclone/domain/retweet/RetweetService.java
+++ src/main/java/com/xclone/xclone/domain/retweet/RetweetService.java
@@ -1,6 +1,8 @@
 package com.xclone.xclone.domain.retweet;
 
 import com.xclone.xclone.domain.notification.NotificationService;
+import com.xclone.xclone.domain.post.PostDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,31 @@ public class RetweetService {
         this.retweetRepository = retweetRepository;
     }
 
+    public ArrayList<Retweet> getAllRetweetsByUserID(Integer retweeterId) {
 
+        return retweetRepository.findAllByRetweeterId(retweeterId);
+
+    }
+
+    @Transactional
+    public void createRetweet(NewRetweet newRetweet) {
+        if (!retweetRepository.existsByRetweeterIdAndReferenceId(newRetweet.retweeterId, newRetweet.referenceId)) {
+            Retweet retweet = new Retweet();
+            retweet.setRetweeterId(newRetweet.retweeterId);
+            retweet.setReferenceId(newRetweet.referenceId);
+            retweet.setType(newRetweet.type);
+            retweetRepository.save(retweet);
+        }
+    }
+
+    @Transactional
+    public void deleteRetweet(NewRetweet newRetweet) {
+        Retweet toDelete = retweetRepository.findByRetweeterIdAndReferenceId(newRetweet.retweeterId, newRetweet.referenceId);
+        if (toDelete != null) {
+            retweetRepository.delete(toDelete);
+        }
+
+    }
 
 
 
