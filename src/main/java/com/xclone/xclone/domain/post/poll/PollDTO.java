685738451065e674af0bbb5a7135e1376d363092
Dave <davidglogowski2003@gmail.com>
2025-07-13 00:44:30 +0200

    chore(poll): getters and setters to poll DTO

--- src/main/java/com/xclone/xclone/domain/post/poll/PollDTO.java
+++ src/main/java/com/xclone/xclone/domain/post/poll/PollDTO.java
@@ -10,4 +10,52 @@ public class PollDTO {
     private Timestamp expiresAt;
     private List<PollChoiceDTO> choices;
     private List<Integer> userVoteChoiceIds; //OPTIONAL FOR CURR USER
+
+    public Long getPollId() {
+        return pollId;
+    }
+
+    public void setPollId(Long pollId) {
+        this.pollId = pollId;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public boolean isMultiVote() {
+        return multiVote;
+    }
+
+    public void setMultiVote(boolean multiVote) {
+        this.multiVote = multiVote;
+    }
+
+    public Timestamp getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Timestamp expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public List<PollChoiceDTO> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<PollChoiceDTO> choices) {
+        this.choices = choices;
+    }
+
+    public List<Integer> getUserVoteChoiceIds() {
+        return userVoteChoiceIds;
+    }
+
+    public void setUserVoteChoiceIds(List<Integer> userVoteChoiceIds) {
+        this.userVoteChoiceIds = userVoteChoiceIds;
+    }
 }
