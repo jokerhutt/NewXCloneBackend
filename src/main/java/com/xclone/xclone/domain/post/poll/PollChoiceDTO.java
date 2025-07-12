352cdb932bced7fb0bdf54f38e09bc7d18560778
Dave <davidglogowski2003@gmail.com>
2025-07-13 00:44:02 +0200

    feat(poll): add poll choice DTO

---
+++ src/main/java/com/xclone/xclone/domain/post/poll/PollChoiceDTO.java
@@ -0,0 +1,34 @@
+package com.xclone.xclone.domain.post.poll;
+
+public class PollChoiceDTO {
+
+    private Long id;
+    private String text;
+    private int voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+}
+
